bdbbd77ef65a61c8aee264f216d945f657b21e6c
JOSHZOL <joshua.rid6873@mediadesign.school.nz>
2017-11-06 21:03:51 +1300

    player->object collisions with restart

--- TOASurfer/Assets/Scripts/ObjectMovementWaves.cs
+++ TOASurfer/Assets/Scripts/ObjectMovementWaves.cs
@@ -27,4 +27,6 @@ public class ObjectMovementWaves : MonoBehaviour {
             Destroy(gameObject);
         }
     }
+
+    
 }
--- TOASurfer/Assets/Scripts/PlayerMovement.cs
+++ TOASurfer/Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerMovement : MonoBehaviour {
-    
+public class PlayerMovement : MonoBehaviour
+{
+
     public float PlayerSpeed;
     public float ArrivalDistance;
 
@@ -13,22 +15,26 @@ public class PlayerMovement : MonoBehaviour {
     Rigidbody rb;
 
     int lane;
+    bool restart = false;
+    float restartTimer = 5.0f;
 
     double SpeedMultiplier;
     float previousXAxis;
-    
+
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
 
         lane = 0;
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-		if (previousXAxis == 0.0f && Input.GetAxis("Horizontal") > 0.0f && lane != 1)
+        // Movement between lanes
+
+        if (previousXAxis == 0.0f && Input.GetAxis("Horizontal") > 0.0f && lane != 1)
         {
             lane++;
         }
@@ -39,6 +45,8 @@ public class PlayerMovement : MonoBehaviour {
 
         previousXAxis = Input.GetAxis("Horizontal");
 
+        // Slow on Arrival to lane
+
         if (Mathf.Abs(rb.position.x - (float)(lane * laneWidth)) > ArrivalDistance)
         {
             SpeedMultiplier = 1.0f;
@@ -47,6 +55,8 @@ public class PlayerMovement : MonoBehaviour {
         {
             SpeedMultiplier = 1.0f * (Mathf.Abs(rb.position.x - (float)(lane * laneWidth)) / ArrivalDistance);
         }
+
+        // Move to lane
 
         if ((lane * laneWidth) > rb.position.x)
         {
@@ -57,6 +67,38 @@ public class PlayerMovement : MonoBehaviour {
             rb.position = new Vector3(rb.position.x - ((float)(PlayerSpeed * SpeedMultiplier) * Time.deltaTime), rb.position.y, rb.position.z);
         }
 
+        // Change height for wave motion
+
         rb.position = new Vector3(rb.position.x, Mathf.Sin((Time.realtimeSinceStartup + (rb.position.z / 10)) * waveSpeed), rb.position.z);
+
+        // Restart after collision
+
+        if (restart == true)
+        {
+            restartTimer -= Time.deltaTime;
+        }
+
+        if (restartTimer <= 0.0f)
+        {
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Obstacle")
+        {
+            restart = true;
+            restartTimer = 2.0f;
+
+            // Play object animation here
+
+            // If needed delete object 
+            Destroy(col.gameObject);
+
+            // Play player animation here
+
+            
+        }
     }
 }
