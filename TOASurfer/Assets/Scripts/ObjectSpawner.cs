92feedd8b8cf10857c7476606fcb4e9528756028
JOSHZOL <joshua.rid6873@mediadesign.school.nz>
2017-11-06 21:30:10 +1300

    created object spawner

--- TOASurfer/Assets/Scripts/ObjectSpawner.cs
+++ TOASurfer/Assets/Scripts/ObjectSpawner.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour {
 
+    public float spawnDelay;
+    public GameObject floatingObject;
+
+    float spawnTimer;
+
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            var newObject = Instantiate(floatingObject);
+            newObject.transform.position = new Vector3(Random.Range(-1, 2) * 3, -3, 25 + (i * 5));
+        }
+
+        
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        spawnTimer -= Time.deltaTime;
+
+        if (spawnTimer <= 0)
+        {
+            spawnTimer = spawnDelay;
+
+            Instantiate(floatingObject);
+
+            floatingObject.transform.position = new Vector3(Random.Range(-1, 2) * 3, -3, 25 + (5 * 5));
+        }
 	}
 }
