394f2fc3241dc9f689aa50c0b86904a4044467ba
Josh Riddlestone <jo5hman@mail.com>
2017-11-08 16:09:05 +1300

    fixed player mesh and added ropes to net

---
+++ TOASurfer/Assets/LineScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineScript : MonoBehaviour {
+
+    LineRenderer line;
+
+    public GameObject hand;
+    public GameObject net;
+
+    // Use this for initialization
+    void Start ()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        line.SetPosition(0, hand.transform.position);
+        line.SetPosition(1, net.transform.position);
+    }
+}
